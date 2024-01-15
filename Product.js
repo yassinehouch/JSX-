4275ccfff47df62358232d59f0e3144374286b81
yassinehouch <yassin.housh@gmail.com>
2024-01-15 13:20:33 +0100

    Added my project

---
+++ Product.js
@@ -0,0 +1,8 @@
+// src/product.js
+export const productData = {
+    name: "Product Name",
+    price: "$19.99",
+    description: "Product description goes here.",
+    imageUrl: "https://example.com/product-image.jpg",
+  };
+  
